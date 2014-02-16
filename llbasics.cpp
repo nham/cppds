00bf6dd13f9cb904c57cf21c825c8cc86292e41c
nham <hamann.nick@gmail.com>
2014-02-16 16:19:35 -0500

    add a tail pointer to SLList. fix a bug in one constructor where it wasnt initializing tail. add add_last() and print().

--- llbasics.cpp
+++ llbasics.cpp
@@ -15,27 +15,67 @@ struct Node {
 };
 
 template <typename T>
-struct LinkedList {
+struct SLList {
     Node<T> *head = nullptr;
+    Node<T> *tail = nullptr;
     int n;
 
-    LinkedList() = default;
-    LinkedList(Node<T> *n) : head{n}, n{n->length()} {}
+    SLList() = default;
+    SLList(Node<T> *n) {
+        this->head = n;
+        auto c = n;
+        int count = 0;
+        for(; c->next != nullptr; c = c->next, ++count);
+        this->tail = c;
+        this->n = count + 1;
+    }
 
-    void addFront(T v) {
+    void add_first(T v) {
         auto newhead = new Node<T>(v);
         newhead->next = this->head;
         this->head = newhead;
+
+        if (n == 0) {
+            this->tail = newhead;
+        }
         this->n += 1;
     }
 
-    T removeFront() {
+    T remove_first() {
         auto oldhead = this->head;
         this->head = this->head->next;
         auto val = oldhead->value;
         delete oldhead;
+
+        if (n == 1) {
+            this->tail = nullptr;
+        }
         this->n -= 1;
         return val;
+    }
+
+    void add_last(T v) {
+        auto newtail = new Node<T>(v);
+        this->tail->next = newtail;
+        this->tail = newtail;
+
+        if (n == 0) {
+            this->head = newtail;
+        }
+        this->n += 1;
+    }
+
+    void print() {
+        if (this->n == 0) {
+            std::cout << '\n';
+            return;
+        }
+
+        std::cout << this->head->value;
+        for (auto c = this->head->next; c != nullptr; c = c->next) {
+            std::cout << " -> " << c->value;
+        }
+        std::cout << '\n';
     }
 };
 
@@ -58,29 +98,37 @@ int main() {
     std::cout << (head->next->next->next == nullptr) << '\n';
     std::cout << "length: " << head->length() << '\n';
 
-    auto list = LinkedList<int>(head);
-    std::cout << "size: " << list.n << '\n';
-    list.addFront(0);
+    auto list = SLList<int>(head);
+    std::cout << "New SLList size: ";
+    list.print();
 
-    std::cout << list.head->value << '\n';
-    std::cout << "new size: " << list.n << '\n';
+    list.add_first(0);
+    std::cout << "after add_first(0): ";
+    list.print();
 
-    std::cout << "-------------\n";
-    auto list2 = LinkedList<bool>();
-    std::cout << list2.n << '\n';
-    list2.addFront(true);
-    std::cout << list2.n << '\n';
-    list2.addFront(false);
-    std::cout << list2.n << '\n';
-    list2.addFront(true);
-    std::cout << list2.n << '\n';
-    list2.addFront(false);
-    std::cout << list2.n << '\n';
+    list.add_last(4);
+    std::cout << "after add_last(4): ";
+    list.print();
 
-    bool tmp;
+    std::cout << "\n-------------\n";
+    auto list2 = SLList<bool>();
+    list2.print();
+    list2.add_first(true);
+    list2.print();
+    list2.add_first(false);
+    list2.print();
+    list2.add_first(true);
+    list2.print();
+    list2.add_first(false);
+    list2.print();
 
-    for(int i = 0; i < 4; i++) {
-        tmp = list2.removeFront();
-        std::cout << "removed " << tmp << ". New size is: " << list2.n << '\n';
-    }
+
+    list2.remove_first();
+    list2.print();
+    list2.remove_first();
+    list2.print();
+    list2.add_last(true);
+    list2.print();
+    list2.remove_first();
+    list2.print();
 }
